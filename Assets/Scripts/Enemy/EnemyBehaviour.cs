4564a9c738a78de30f3cad4ea12ad78a99a2ab40
Lukas Bijelic <Bijelic.Lukas.2003@gmail.com>
2025-11-07 21:01:34 +0100

    Player & Enemy Interaction
    The Player and Enemy now function correctly.

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] GameObject Player;
-    [SerializeField] float Speed;
+    [SerializeField] PlayerData playerData;
+    [SerializeField] float BasicSpeed;
+
+    float speedToleranz = 2.5f;
+    float speed;
+
+    private void Start()
+    {
+        speed = Random.Range(BasicSpeed - speedToleranz, BasicSpeed + speedToleranz * 0.1f);
+    }
 
     private void Update()
     {
-        Vector3 direction = Vector3.Normalize(Player.transform.position - transform.position);
+        Vector3 direction = Vector3.Normalize(playerData.playerPosition - transform.position);
         direction.y = 0;
         //Debug.Log($"Direction: {direction}");
         if(direction.x != 0 && direction.z != 0)
         {
             transform.forward = direction;
-            transform.position += direction * Time.deltaTime * Speed;
+            transform.position += direction * Time.deltaTime * speed;
         }
     }
 
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] PlayerData data;
     Vector3 movement = new Vector3();
 
     private void Update()
     {
         transform.position += movement * Time.deltaTime * speed;
+        data.playerPosition = transform.position;
     }
 
     public void GetPlayerMovement(InputAction.CallbackContext context)
